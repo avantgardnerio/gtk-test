7dee09a0088607584b40d24a77174ef609c03781
Brent Gardner <bgardner@squarelabs.net>
2024-04-28 10:02:55 -0400

    can load parquet files

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ extern crate gio;
 use gtk::prelude::*;
 use gio::prelude::*;
 
-use gtk::{Builder,Window, Button};
+use gtk::{Builder, Window, Button, MenuItem, FileChooserDialog};
 
 use std::env::args;
+use std::fs::File;
+use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
 
 // the handler
 fn on_clicked(param: &[glib::Value]) -> Option<glib::Value> {
@@ -20,6 +22,33 @@ fn build_ui(application: &gtk::Application) {
 
     let window: Window = builder.get_object("window1").expect("Couldn't get window");
 
+    let file_chooser: FileChooserDialog = builder.get_object("fileChooser1").expect("No chooser");
+    file_chooser.set_application(Some(application));
+
+    file_chooser.connect_file_activated(|chooser| {
+        let file = chooser.get_file().unwrap();
+        let path = file.get_path().unwrap();
+        println!("file={file:?}");
+
+        let file = File::open(path).unwrap();
+
+        let builder = ParquetRecordBatchReaderBuilder::try_new(file).unwrap();
+        println!("Converted arrow schema is: {}", builder.schema());
+
+        let mut reader = builder.build().unwrap();
+
+        let record_batch = reader.next().unwrap().unwrap();
+        println!("batch has {} rows", record_batch.num_rows());
+
+        chooser.hide();
+    });
+
+    let btn: MenuItem = builder.get_object("button1").expect("Cant get button");
+    btn.connect_activate(move |_| {
+        file_chooser.show_all();
+        println!("Activated");
+    });
+
     window.set_application(Some(application));
     window.set_title("Test");
 
@@ -27,13 +56,6 @@ fn build_ui(application: &gtk::Application) {
         gtk::main_quit();
         Inhibit(true)
     });
-
-    // directly calling the button1 without implementing signal
-    let btn: Button = builder.get_object("button1").expect("Cant get button");
-    btn.connect_clicked(|_| {
-    println!("Activated");
-    });
-
 
     // builder.connect_signals(|builder, handler_name| {
     //     match handler_name {
