ead00f4a171d28d81dd6d42eb8a0e230870a5e2f
Brent Gardner <bgardner@squarelabs.net>
2024-04-28 10:29:03 -0400

    Can display list

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,28 @@ extern crate gio;
 use gtk::prelude::*;
 use gio::prelude::*;
 
-use gtk::{Builder, Window, Button, MenuItem, FileChooserDialog};
+use gtk::{Builder, Window, Button, MenuItem, FileChooserDialog, TreeView, TreeStore, TreeViewColumn, CellRendererText};
 
 use std::env::args;
 use std::fs::File;
+use gdk::gio::ListStore;
+use gio::ListStoreBuilder;
 use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
 
 // the handler
 fn on_clicked(param: &[glib::Value]) -> Option<glib::Value> {
     println!("on_start_clicked fired!");
     None
+}
+
+fn append_text_column(tree: &TreeView) {
+    let column = TreeViewColumn::new();
+    let cell = CellRendererText::new();
+
+    column.set_title("Text");
+    column.pack_start(&cell, true);
+    column.add_attribute(&cell, "text", 0);
+    tree.append_column(&column);
 }
 
 fn build_ui(application: &gtk::Application) {
@@ -48,6 +60,19 @@ fn build_ui(application: &gtk::Application) {
         file_chooser.show_all();
         println!("Activated");
     });
+
+    let left_tree: TreeView = builder.get_object("treeView1").expect("No TreeView");
+    let left_store = TreeStore::new(&[String::static_type()]);
+    left_tree.set_model(Some(&left_store));
+    append_text_column(&left_tree);
+    left_tree.set_headers_visible(true);
+
+    for i in 0..10 {
+        // insert_with_values takes two slices: column indices and ToValue
+        // trait objects. ToValue is implemented for strings, numeric types,
+        // bool and Object descendants
+        let _ = left_store.insert_with_values(None, None, &[0], &[&format!("Hello {}", i)]);
+    }
 
     window.set_application(Some(application));
     window.set_title("Test");
